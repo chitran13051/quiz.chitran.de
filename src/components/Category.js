3a88b16da43e896bb5b453e8d5794b4f4ee75199
Chi Tran <khanhchi.tran13051@gmail.com>
2021-05-22 09:26:05 +0200

    try to fix page not found on netlify

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,16 +3,6 @@ import { useSelector, useDispatch } from "react-redux"
 import { CategoryStyle } from "../Stylings"
 import { selectCategory } from "../redux/quiz.actions"
 
-const categoriesNumber = [null, 17, 18, 21, 22, 23, 25]
-const categoriesName = [
-  "All",
-  "Science",
-  "Computers",
-  "Sports",
-  "Geography",
-  "History",
-  "Art",
-]
 const options = [
   { value: 17, label: "Science" },
   { value: 18, label: "Computers" },
@@ -24,7 +14,7 @@ const options = [
 
 const customStyles = {
   /**
-   * @ https://github.com/JedWatson/react-select/issues/2728
+   * @Thanks https://github.com/JedWatson/react-select/issues/2728
    * customize outline color
    */
   control: (base, state) => ({
@@ -49,23 +39,4 @@ export default function Category() {
       placeholder="All"
     />
   )
-  {
-    /* <select
-        onChange={e => {
-          console.log(e.target.value)
-          const cateId = categoriesNumber[e.target.value]
-          dispatch(selectCategory(cateId))
-        }}
-      >
-        {categoriesName.map((cate, i) => (
-          <option key={i} value={i}>
-            {" "}
-            {cate}{" "}
-          </option>
-        ))}
-      </select> */
-  }
-  {
-    /* </CategoryStyle> */
-  }
 }
