c9c8ef08afed0e914ef90cd68f37ebd0a796f8dc
Chi Tran <khanhchi.tran13051@gmail.com>
2021-05-23 20:03:18 +0200

    try SEO with react-helmet

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -2,7 +2,18 @@ import React from "react"
 import { Helmet } from "react-helmet"
 
 const SEO = ({ title }) => {
-  return <Helmet title={title} />
+  return (
+    <Helmet
+      title={title}
+      meta={[
+        { name: "description", content: "A trivia game by chitran.de" },
+        {
+          name: "author",
+          content: "https://chitran.de",
+        },
+      ]}
+    />
+  )
 }
 
 export default SEO
